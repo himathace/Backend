da54cae78819bc41457b5127f313dd0c741de526
himath <himathcode100@gmail.com>
2025-07-15 19:18:24 +0530

    add jwt

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express=require("express")
 const app=express()
+const jwt=require("jsonwebtoken")
+const bodyparser=require("body-parser")
+require('dotenv').config();
 app.use(express.json())
 
 
@@ -8,9 +11,6 @@ const user={
     password:"1234"
 }
 
-app.get("/pages",(req,res)=>{
-    res.json({message:"hello"})
-})
 
 app.post("/login",(req,res)=>{
 
@@ -18,11 +18,33 @@ app.post("/login",(req,res)=>{
     const password=req.body.password
 
     if(username == user.name && password==user.password){
-        res.json({message:"welcome"})
+        const token=jwt.sign({name:user.name},process.env.serect_key)
+        res.json({jwttoken:token})
     }
     else{
         res.json({message:"invalid username or password"})
     }
+})
+
+
+app.get("/profile",(req,res)=>{
+
+    const authheader=req.headers["authorization"]
+    const token=authheader && authheader.split(" ")[1]
+
+    if(!token){
+        return res.json({message:"token missing"})
+    }
+
+    jwt.verify(token,process.env.serect_key,(error,value)=>{
+        if(error){
+            return res.json({message:"invlaid user"})
+        }
+
+        res.json({message:"welcome to your profile",value})
+
+    })
+
 })
 
 
