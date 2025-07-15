13a066b9ebb26295cf174a9c15f358bff5290a13
himath <himathcode100@gmail.com>
2025-07-15 18:16:11 +0530

    create login

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express=require("express")
 const app=express()
+app.use(express.json())
 
+
+const user={
+    name:"himath",
+    password:"1234"
+}
 
 app.get("/pages",(req,res)=>{
     res.json({message:"hello"})
 })
+
+app.post("/login",(req,res)=>{
+
+    const username=req.body.username
+    const password=req.body.password
+
+    if(username == user.name && password==user.password){
+        res.json({message:"welcome"})
+    }
+    else{
+        res.json({message:"invalid username or password"})
+    }
+})
+
+
+
+
+
+
 
 app.listen(3000)
